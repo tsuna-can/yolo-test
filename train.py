b137bb1880b3562c264e4efb31a5240e82ca0174
tsuna-can <sin2016iba@gmail.com>
2020-09-21 16:57:43 +0900

    add ipynb

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 """
 Retrain the YOLO model for your own dataset.
+画像サイズとパッチサイズとanchorを変更する
 """
 
 import numpy as np
@@ -17,7 +18,7 @@ def _main():
     annotation_path = 'model_data/2007_train.txt'
     log_dir = 'logs/000/'
     classes_path = 'model_data/voc_classes.txt'
-    anchors_path = 'model_data/tiny_yolo_anchors.txt'
+    anchors_path = 'model_data/tiny_yolo_anchors.txt'#tinyを使わないなら変更
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
